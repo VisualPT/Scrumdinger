9ffc08bbae8eb4d34d556390e8d67f14a5c64d38
charlieforward9 <62311337+charlieforward9@users.noreply.github.com>
2023-01-05 23:13:36 -0500

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Charles Richardson on 1/5/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
