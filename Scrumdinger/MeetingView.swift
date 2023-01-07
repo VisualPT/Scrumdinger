fae9a5eb96223e0a0520c06d444065dadaa3ca14
charlieforward9 <62311337+charlieforward9@users.noreply.github.com>
2023-01-07 00:12:31 -0500

    Swift UI Essentials Done

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -12,16 +12,29 @@ struct MeetingView: View {
         VStack {
             ProgressView(value: 5, total: 15)
             HStack{
-                VStack {
-                    Text("Seconds Elapsed")
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed").font(.caption)
                     Label("300", systemImage: "hourglass.bottomhalf.fill")
                 }
-                VStack {
-                    Text("Seconds Remaining")
+                Spacer()
+                VStack(alignment: .leading) {
+                    Text("Seconds Remaining").font(.caption)
                     Label("600", systemImage: "hourglass.tophalf.fill")
                 }
             }
-        }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time Remaining")
+            .accessibilityValue("10 minutes")
+            Circle().strokeBorder(lineWidth: 24)
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+                .accessibilityLabel("Next speaker")
+            }
+        }.padding()
     }
 }
 
